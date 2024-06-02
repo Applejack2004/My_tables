05b1f30e04803426c7e94b770727326a20381f4d
4885226 <ivan.alputov@mail.ru>
2024-06-02 18:13:19 +0300

    финальная версия

--- Tests/test_main.cpp
+++
@@ -1,9 +0,0 @@
-#define _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING
-#include "gtest.h"
-
-int main(int argc, char **argv)
-{
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-  //cfv kmvwmvwvmmea
-}
